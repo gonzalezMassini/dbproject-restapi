510bcd055be5682a44f9996a2b5428271864f450
Jose Gonzalez-Massini <74879546+gonzalezMassini@users.noreply.github.com>
2021-10-12 20:11:16 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_mysqldb import MySQL
 from controllers.userCreation import user_creation
 from controllers.getAllUsers import get_all_users
